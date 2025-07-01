2c3d76fa899d245096fbfb6b71616e267b1d1078
Misha Kolosov <mikhail230306@mail.ru>
2025-07-01 16:12:25 +0300

    made 1st

--- mod6/6_7/test.py
+++ mod6/6_7/test.py
@@ -1,10 +1,8 @@
 from collections import Counter
 
+vegetables = Counter({'cabbage': 10, 'pepper': 'seven', 'pumpkin': 'four'})
 
-Counter('Sandy Cheeks')
-Counter('Sandy Cheeks'.split())
-Counter(Sandy=1.5, Cheeks=-3)
-Counter({'S': 1, 'a': 5, 'n': 3, 'd': 9, 'y': 7})
-Counter(Sandy='2', Cheeks='4')
-Counter({'S': '1', 'a': '5', 'n': '3', 'd': '9', 'y': '7'})
-Counter.fromkeys('Sandy', 1)
+vegetables.update({'cabbage': 5, 'pepper': 'two'})
+
+print(vegetables['pepper'])
+print(vegetables['cabbage'])
